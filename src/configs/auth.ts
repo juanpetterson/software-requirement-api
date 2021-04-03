62859571179647ad7f7863f01fc7d8ccab1f7dc0
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-04-03 19:13:55 -0300

    add auth configuration and middleware

---
+++ src/configs/auth.ts
@@ -0,0 +1,4 @@
+export default {
+  secret: process.env.TOKEN_SECRET,
+  ttl: process.env.TOKEN_TTL,
+};
---
+++ src/middlewares/auth.ts
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import authConfig from "../configs/auth";
+import { promisify } from "util";
+import { NextFunction, Request, Response } from "express";
+
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Token not provided" });
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    await promisify(jwt.verify)(token, authConfig.secret);
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: "Token invalid" });
+  }
+};
+
+export default authMiddleware;

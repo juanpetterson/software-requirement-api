fcd6a55e43a7a243d398a0bc77df102067590c3e
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-04-11 21:49:20 -0300

    update user model typings

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,13 +2,27 @@ import { Schema, model } from 'mongoose';
 import IUser from '../interfaces/user';
 import logging from '../configs/logging';
 
+const RequiredString = {
+  type: String,
+  required: true,
+};
+
 const UserSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  isAdmin: Boolean,
-  createdAt: Date,
-  updatedAt: Date,
+  name: RequiredString,
+  email: RequiredString,
+  password: RequiredString,
+  isAdmin: {
+    type: Boolean,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+  },
 });
 
 UserSchema.post<IUser>('save', function () {
